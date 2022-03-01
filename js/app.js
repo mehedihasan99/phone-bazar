20af3d0093450c6d8ccb1b34ed9666224e0124b8
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-03-02 02:06:45 +0600

    spinner with error handling

--- js/app.js
+++ js/app.js
@@ -4,19 +4,23 @@ const errorMsg = document.getElementById("error-msg");
 function displaySpinner(style){
     document.getElementById("spinner").style.display = style;
 }
+// --------- card group visibility style---------
+function displayPhoneStyle(cardStyle){
+    document.getElementById("card-group").style.visibility = cardStyle;
+}
 // ------------------------search button-----------
 const searchPhone = () =>{
     const inputField = document.getElementById("input-field");
     const inputValue = inputField.value ;
     inputField.value = '';
+    // dynamic url with case sensitive
     const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue.toLowerCase()}`
     fetch(url)
     .then(res => res.json())
     .then(data => {
         // --------- error msg condition-------
-        if(data.data.length == 0){
-            errorMsg.innerText = "Please Input a Valid Value";
-            
+        if(data.data.length == 0 || data.data == ""){
+            errorMsg.innerText = "Please Input a Valid Value"; 
         }
         else{
             errorMsg.innerText = "";
@@ -26,26 +30,22 @@ const searchPhone = () =>{
     displaySpinner("block") ;
     displayPhoneStyle("hidden");
 }
-function displayPhoneStyle(style){
-    document.getElementById("card-group").style.visibility = style;
-}
-// display phone /
 
+//---------------- display phone ---------
 const displayPhone = phones =>{
     // display 20 items 
     const phoneList = phones.slice(0, 20);
     const cardGroup = document.getElementById("card-group");
+    cardGroup.textContent = "";
     phoneList.forEach( phone =>{
-    // console.log(phone.slug);
     const div = document.createElement("div");
-    div.className = "col-lg-4";
-    // console.log(phone.brand);
+    div.className = "col-md-4";
     div.innerHTML = `
     <div class="card mb-4">
         <div class="text-center mt-3">
         <img src="${phone.image}" class="card-img-top w-50"  alt="...">
         </div>
-        <div class="card-body ">
+        <div class="card-body text-center">
         <h5 class="card-title">${phone.phone_name}</h5>
         <h5 class="card-title">${phone.brand}</h5>
         <button onclick="phoneDetails('${phone.slug}')" href="#" class="btn btn-primary mt-3">Details</button>
@@ -69,16 +69,15 @@ const phoneDetails = details =>{
 // display phone details
 
 const displayPhoneDetails = phoneDetails =>{
-    // console.log(phoneDetails.others);
     // ------------------- phone detail container---
     const phoneDetail = document.getElementById("phone-detail");
     phoneDetail.textContent = "";
     // ---------- create new date and append--
     const div = document.createElement("div");
-    div.className = "col-lg-12";
+    div.className = "col-md-12";
     div.innerHTML = `
     <div class="card my-3 mx-auto" style="max-width: 640px;">
-  <div class="row g-0">
+  <div class="row g-0 p-3">
     <div class="col-md-4">
       <img src="${phoneDetails.image}" class="img-fluid rounded-start" alt="...">
     </div>
